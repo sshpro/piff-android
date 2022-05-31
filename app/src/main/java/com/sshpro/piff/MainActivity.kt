1413fcead80a9621477396f87d2394842428d243
sshpro <sshpro9@gmail.com>
2022-05-31 17:54:37 +0100

    chore(PIFF-0): extracted composable out to own functions in MainActivity

--- app/src/main/java/com/sshpro/piff/MainActivity.kt
+++ app/src/main/java/com/sshpro/piff/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
@@ -36,47 +38,56 @@ class MainActivity : ComponentActivity() {
             PiffTheme {
                 val navController = rememberNavController()
                 val backstackEntry = navController.currentBackStackEntryAsState()
+                val currentScreen = PhotoScreen.fromRoute(backstackEntry.value?.destination?.route)
                 Scaffold(
                     topBar = {
-                        PhotoTopView(currentScreen = PhotoScreen.fromRoute(backstackEntry.value?.destination?.route))
+                        PhotoTopView(currentScreen = currentScreen)
                     }
                 ) { innerPadding ->
-                    NavHost(
-                        navController = navController,
-                        startDestination = PhotoScreen.Gallery.name,
-                        modifier = Modifier.padding(innerPadding)
-                    ) {
-                        composable(PhotoScreen.Gallery.name) {
-                            viewModel.getPhotos()
-                            val dataState = viewModel.getPhotosDataState.value
-
-                            GalleryView(dataState = dataState, onPhotoClick = { photoUrl ->
-                                val encodedUrl = URLEncoder.encode(photoUrl, StandardCharsets.UTF_8.toString())
-                                navigateToPhotoDetail(navController, encodedUrl)
-                            })
-                        }
-                        val photoName = PhotoScreen.Detail.name
-                        composable(
-                            route = "$photoName/{encodedUrl}",
-                            arguments = listOf(
-                                navArgument("encodedUrl") {
-                                    // Make argument type safe
-                                    type = NavType.StringType
-                                }
-                            )
-                        ) { entry ->
-                            val encodedUrl = entry.arguments?.getString("encodedUrl") ?: ""
-                            val decodedUrl = URLDecoder.decode(encodedUrl, StandardCharsets.UTF_8
-                                .toString())
-                            viewModel.getPhoto(decodedUrl)
-                            PhotoDetailView(dataState = viewModel.getPhotoDataState.value)
-                        }
-                    }
+                    NavigationComponent(innerPadding = innerPadding, navController = navController)
                 }
             }
         }
     }
+
+    @Composable
+    private fun NavigationComponent(innerPadding: PaddingValues, navController: NavHostController) {
+        NavHost(
+            navController = navController,
+            startDestination = PhotoScreen.Gallery.name,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(PhotoScreen.Gallery.name) {
+                viewModel.getPhotos()
+                val dataState = viewModel.getPhotosDataState.value
+
+                GalleryView(dataState = dataState, onPhotoClick = { photoUrl ->
+                    val encodedUrl = URLEncoder.encode(photoUrl, StandardCharsets.UTF_8.toString())
+                    navigateToPhotoDetail(navController, encodedUrl)
+                })
+            }
+            val photoName = PhotoScreen.Detail.name
+            composable(
+                route = "$photoName/{encodedUrl}",
+                arguments = listOf(
+                    navArgument("encodedUrl") {
+                        // Make argument type safe
+                        type = NavType.StringType
+                    }
+                )
+            ) { entry ->
+                val encodedUrl = entry.arguments?.getString("encodedUrl") ?: ""
+                val decodedUrl = URLDecoder.decode(
+                    encodedUrl, StandardCharsets.UTF_8
+                        .toString()
+                )
+                viewModel.getPhoto(decodedUrl)
+                PhotoDetailView(dataState = viewModel.getPhotoDataState.value)
+            }
+        }
+    }
 }
+
 
 private fun navigateToPhotoDetail(navController: NavHostController, photoUrl: String) {
     navController.navigate("${PhotoScreen.Detail.name}/$photoUrl")
